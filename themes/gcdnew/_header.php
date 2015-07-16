85c0f8fc01db88279e8c188c36a2fd625a44ed56
unknown <Teerawat@.(none)>
2015-07-16 16:35:34 +0700

    update menu

--- themes/gcdnew/_header.php
+++ themes/gcdnew/_header.php
@@ -67,11 +67,16 @@
 	<ul id="drop-nav">
 	  <li><a target="_self" href="home"><?php echo lang('home')?></a></li>
 	  <li><a target="_self" href="pages/aboutus"><?php echo lang('about')?></a>
-	    <!-- <ul>
-	      <li><a href="#">HTML</a></li>
-	      <li><a href="#">CSS</a></li>
-	      <li><a href="#">JavaScript</a></li>
-	    </ul> -->
+	    <ul>
+	      <li><a href="pages/department_history">ประวัติสำนัก</a></li>
+	      <li><a href="pages/department_vision">วิสัยทัศน์</a></li>
+	      <li><a href="pages/department_mission">พันธกิจ</a></li>
+	      <li><a href="pages/department_strategy">กลยุทธ์</a></li>
+	      <li><a href="pages/department_tasks">ภารกิจ</a></li>
+	      <li><a href="pages/department_structure">โครงสร้างหน่วยงาน</a></li>
+	      <li><a href="pages/department_location">ที่ตั้งหน่วยงาน</a></li>
+	      <li><a href="pages/department_plan">แผนปฏิบัติราชการ</a></li>
+	    </ul>
 	  </li>
 	  <li><a target="_self" href="executives"><?php echo lang('executives')?></a></li>
 	  <li><a target="_self" href="groups"><?php echo lang('structures')?></a>
